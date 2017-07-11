5c56b48a2dfaf0e1a3da548bd84a9297ffb4129d
orhanberamus <orhannfidann@gmail.com>
2017-07-11 18:33:03 +0300

    added skyscraper

---
+++ skyscraper/match.rb
@@ -0,0 +1,31 @@
+lyrics = "I eat my dinner in my bathtub
+Then I go to sex clubs
+Watching freaky people getting it on
+It doesn't make me nervous
+If anything, I'm restless
+Yeah, I've been around and I've seen it all
+I get home, I got the munchies
+Binge on all my Twinkies
+Throw up in the tub and I go to sleep
+And I drank up all my money
+Dazed and kinda lonely
+
+[Chorus]
+You're gone and I got to stay high
+All the time, to keep you off my mind
+Oh oh oh oh, oh oh oh oh
+High, all the time, to keep you off my mind
+Oh oh oh oh, oh oh oh oh
+Spend my days locked in a haze
+Trying to forget you, babe
+I fall back down
+I gotta stay high, all my life
+To forget I'm missing you
+Oh oh oh oh, oh oh oh oh"
+
+part =nil
+if !part.nil?
+    puts "nil degil"
+else
+    puts "nil"
+end
---
+++ skyscraper/scraper.rb
@@ -0,0 +1,77 @@
+require "watir"
+require "nokogiri"
+lyrics = ""
+artist = ""
+song_name = ""
+genius = "genius"
+while lyrics = "" || artist = "" ||  song_name = "" do
+   
+
+browser = Watir::Browser.new :chrome
+#browser.minimize
+browser.goto "http://www.google.com/"
+browser.text_field(title: "Ara").set"pink floyd hey you #{genius}"
+browser.button(type: "submit").click
+browser.h3s(class: 'r')[0].click 
+doc = Nokogiri::HTML.parse(browser.html)
+
+#browser.screenshot.save 'screenshots\search-results.png'
+doc.css('br').each{ |br| br.replace "{" }
+lyrics = doc.css('div.lyrics section p').text
+artist = doc.css('a.header_with_cover_art-primary_info-primary_artist').text
+song_name = doc.css('h1.header_with_cover_art-primary_info-title').text
+puts song_name 
+
+end
+
+
+part = Array.new(140, "")
+
+chorus = "Chorus"
+verse = "Verse"
+string = "{"
+
+if !lyrics.index(chorus).nil?
+    position = lyrics.index(chorus)
+    i = 0
+    count = 0
+    while lyrics[position + i ] != string do
+        i += 1 
+    end
+    while(count < 3  && i < 140) do
+        part[i] = lyrics[position +i]
+        if (lyrics[position +i] == string)
+          count+= 1
+        end
+        i += 1
+    end
+elsif !lyrics.index(verse).nil?
+    position = lyrics.index(verse)
+    i = 0
+    count = 0
+    while lyrics[position + i ] != string do
+        i += 1 
+    end
+    while(count < 3  && i < 140) do
+        part[i] = lyrics[position +i]
+        if (lyrics[position +i] == string)
+          count+= 1
+        end
+        i += 1
+    end
+end
+
+
+  def  artistModifier
+          
+    
+  end
+
+
+
+stringpart = part.join("")
+stringpart.gsub!('{','\n')
+puts stringpart
+
+
+  
